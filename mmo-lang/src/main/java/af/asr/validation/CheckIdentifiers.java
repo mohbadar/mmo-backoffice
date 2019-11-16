c6613b104559b04fadbfd0f8ac43f6778e22fb75
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-16 16:49:55 +0430

    MMO Lang Library => Validation Section

--- mmo-lang/src/main/java/af/asr/validation/CheckIdentifiers.java
+++ mmo-lang/src/main/java/af/asr/validation/CheckIdentifiers.java
@@ -1,4 +1,29 @@
 package af.asr.validation;
 
-public class CheckIdentifiers {
+
+import af.asr.validation.constaints.ValidIdentifiers;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+
+public class CheckIdentifiers implements ConstraintValidator<ValidIdentifiers, List<String>> {
+    private int maximumLength = 32;
+    private boolean optional;
+
+    @Override
+    public void initialize(final ValidIdentifiers constraintAnnotation) {
+        maximumLength = constraintAnnotation.maxLength();
+        optional = constraintAnnotation.optional();
+
+    }
+
+    @Override
+    public boolean isValid(final List<String> value, final ConstraintValidatorContext context) {
+        if (optional && value == null)
+            return true;
+
+        return value != null && value.stream().allMatch(x -> x != null && CheckIdentifier.validate(x, maximumLength));
+    }
 }
