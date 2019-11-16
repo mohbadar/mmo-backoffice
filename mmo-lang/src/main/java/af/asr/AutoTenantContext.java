322f8d15fa54311719e1f47a6b9860dcf38a95af
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-16 16:26:43 +0430

    MMO Lang Library and Lang Data Setup

--- mmo-lang/src/main/java/af/asr/AutoTenantContext.java
+++ mmo-lang/src/main/java/af/asr/AutoTenantContext.java
@@ -1,4 +1,28 @@
 package af.asr;
 
-public class AutoTenantContext {
+import java.util.Optional;
+
+@SuppressWarnings({"OptionalUsedAsFieldOrParameterType", "WeakerAccess", "unused"})
+public class AutoTenantContext implements AutoCloseable {
+
+    private final Optional<String> previousIdentifier;
+
+    public AutoTenantContext(final String tenantName)
+    {
+        previousIdentifier = TenantContextHolder.identifier();
+        TenantContextHolder.clear();
+        TenantContextHolder.setIdentifier(tenantName);
+    }
+
+    public AutoTenantContext()
+    {
+        previousIdentifier = TenantContextHolder.identifier();
+        TenantContextHolder.clear();
+    }
+
+    @Override
+    public void close() {
+        TenantContextHolder.clear();
+        previousIdentifier.ifPresent(TenantContextHolder::setIdentifier);
+    }
 }
