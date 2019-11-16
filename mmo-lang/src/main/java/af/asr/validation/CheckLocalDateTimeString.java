e4154c053e5c268b26cb2ea3c225dfda942ae840
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-16 16:30:22 +0430

    MMO Lang Library => Validation Section

---
+++ mmo-lang/src/main/java/af/asr/validation/CheckLocalDateTimeString.java
@@ -0,0 +1,24 @@
+package af.asr.validation;
+
+
+import af.asr.DateConverter;
+
+import java.time.DateTimeException;
+
+
+public class CheckLocalDateTimeString implements ConstraintValidator<ValidLocalDateTimeString, String> {
+    public void initialize(ValidLocalDateTimeString constraint) {
+    }
+
+    public boolean isValid(final String obj, ConstraintValidatorContext context) {
+        if (obj == null)
+            return true;
+        try {
+            DateConverter.fromIsoString(obj);
+            return true;
+        }
+        catch (final DateTimeException e) {
+            return false;
+        }
+    }
+}
