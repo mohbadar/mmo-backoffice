b02e92295d2cbb5f5d223901921ac8f0344d33a7
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-16 16:25:58 +0430

    MMO Lang Library and Lang Data Setup

---
+++ mmo-lang/src/main/java/af/asr/AutoTenantContext.java
@@ -0,0 +1,4 @@
+package af.asr;
+
+public class AutoTenantContext {
+}
--- mmo-lang/src/main/java/af/asr/TenantContextHolder.java
+++ mmo-lang/src/main/java/af/asr/TenantContextHolder.java
@@ -1,4 +1,35 @@
 package af.asr;
 
-public class TenantContextHolder {
+import org.springframework.util.Assert;
+
+import javax.annotation.Nonnull;
+import java.util.Optional;
+
+public final class TenantContextHolder {
+
+    private static final InheritableThreadLocal<String> THREAD_LOCAL = new InheritableThreadLocal<>();
+
+    private TenantContextHolder() {
+        super();
+    }
+
+    @Nonnull
+    public static Optional<String> identifier() {
+        return Optional.ofNullable(TenantContextHolder.THREAD_LOCAL.get());
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public static String checkedGetIdentifier() {
+        return identifier().orElseThrow(() -> new IllegalStateException("Tenant context not set."));
+    }
+
+    public static void setIdentifier(@Nonnull final String identifier) {
+        Assert.hasLength(identifier, "A tenant identifier must have at least one character.");
+        Assert.isNull(TenantContextHolder.THREAD_LOCAL.get(), "Tenant identifier already set.");
+        TenantContextHolder.THREAD_LOCAL.set(identifier);
+    }
+
+    public static void clear() {
+        TenantContextHolder.THREAD_LOCAL.remove();
+    }
 }
