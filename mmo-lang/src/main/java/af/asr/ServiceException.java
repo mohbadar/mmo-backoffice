37b5ce205b3ec1b57518421b7acf5b4bf523116c
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-16 16:24:11 +0430

    MMO Lang Library and Lang Data Setup

--- mmo-lang/src/main/java/af/asr/ServiceException.java
+++ mmo-lang/src/main/java/af/asr/ServiceException.java
@@ -1,4 +1,55 @@
 package af.asr;
 
-public class ServiceException {
+import java.text.MessageFormat;
+
+@SuppressWarnings({"WeakerAccess", "unused"})
+public final class ServiceException extends RuntimeException {
+
+    private final ServiceError serviceError;
+
+    public ServiceException(final ServiceError serviceError) {
+        super(serviceError.getMessage());
+        this.serviceError = serviceError;
+    }
+
+    public static ServiceException badRequest(final String message, final Object... args) {
+        return new ServiceException(ServiceError
+                .create(400)
+                .message(MessageFormat.format(message, args))
+                .build());
+    }
+
+    public static ServiceException notFound(final String message, final Object... args) {
+        return new ServiceException(ServiceError
+                .create(404)
+                .message(MessageFormat.format(message, args))
+                .build());
+    }
+
+    public static ServiceException conflict(final String message, final Object... args) {
+
+        return new ServiceException(ServiceError
+                .create(409)
+                .message(MessageFormat.format(message, args))
+                .build());
+    }
+
+    public static ServiceException internalError(final String message, final Object... args) {
+        return new ServiceException(ServiceError
+                .create(500)
+                .message(MessageFormat.format(message, args))
+                .build());
+    }
+
+    public ServiceError serviceError() {
+        return this.serviceError;
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceException{" +
+                "serviceError=" + serviceError +
+                '}';
+    }
+
 }
