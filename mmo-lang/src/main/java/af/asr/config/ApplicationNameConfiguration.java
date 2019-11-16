8dd4203c0243300f7b03959ee2262225f0b7135b
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-16 16:53:01 +0430

    MMO Lang Library => Configuration Section

--- mmo-lang/src/main/java/af/asr/config/ApplicationNameConfiguration.java
+++ mmo-lang/src/main/java/af/asr/config/ApplicationNameConfiguration.java
@@ -1,4 +1,17 @@
 package af.asr.config;
 
+import af.asr.ApplicationName;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@SuppressWarnings("WeakerAccess")
+@Configuration
 public class ApplicationNameConfiguration {
+    @Bean
+    public ApplicationName applicationName(
+            @Value("${spring.application.name}") final String springApplicationName) {
+        return ApplicationName.fromSpringApplicationName(springApplicationName);
+    }
 }
