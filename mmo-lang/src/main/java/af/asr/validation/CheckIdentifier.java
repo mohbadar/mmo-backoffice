e0bb60954225f30877c59f5da9e3a94e9f8cfed3
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-16 16:49:33 +0430

    MMO Lang Library => Validation Section

---
+++ mmo-lang/src/main/java/af/asr/validation/CheckIdentifier.java
@@ -0,0 +1,46 @@
+package af.asr.validation;
+
+import af.asr.validation.constaints.ValidIdentifier;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+
+public class CheckIdentifier implements ConstraintValidator<ValidIdentifier, String> {
+    private int maximumLength = 32;
+    private boolean optional = false;
+    @Override
+    public void initialize(final ValidIdentifier constraint) {
+        maximumLength = constraint.maxLength();
+        optional = constraint.optional();
+    }
+
+    @Override
+    public boolean isValid(final String obj, final ConstraintValidatorContext context) {
+        if (obj == null)
+            return optional;
+
+        return validate(obj, maximumLength);
+    }
+
+    static boolean validate(final String obj, final int maximumLength) {
+        if (obj.length() < 2)
+            return false;
+
+        if (obj.length() > maximumLength)
+            return false;
+
+        try {
+            return encode(obj).equals(obj);
+        }
+        catch (UnsupportedEncodingException e) {
+            return false; //If we can't encode with UTF-8, then there are no valid names.
+        }
+    }
+
+    static private String encode(String identifier) throws UnsupportedEncodingException {
+        return URLEncoder.encode(identifier, "UTF-8");
+    }
+}
