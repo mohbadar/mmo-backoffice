60339677995f08270cf30f4f818797819f99a90a
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-16 16:54:26 +0430

    MMO Lang Library => Configuration Section

---
+++ mmo-lang/src/main/java/af/asr/config/EnableServiceJavaConfiguration.java
@@ -0,0 +1,23 @@
+package af.asr.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceExceptionJavaConfiguration {
+
+    public ServiceExceptionJavaConfiguration() {
+        super();
+    }
+
+    @Bean
+    public FilterRegistrationBean serviceExceptionFilterRegistration() {
+        final ServiceExceptionFilter serviceExceptionFilter = new ServiceExceptionFilter();
+        final FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(serviceExceptionFilter);
+        registration.addUrlPatterns("/*");
+        registration.setName("serviceExceptionFilter");
+        return registration;
+    }
+}
