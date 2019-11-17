ddb3fae5abfd15f00c344e3735f87a38a8150427
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-17 09:28:13 +0430

    MMO Sync Library Setup

--- mmo-async/src/main/java/af/asr/async/core/DelegatingContextCallable.java
+++ mmo-async/src/main/java/af/asr/async/core/DelegatingContextCallable.java
@@ -1,36 +1,36 @@
-package af.asr.async.core;
-
-import java.util.Optional;
-import java.util.concurrent.Callable;
-
-@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-public class DelegatingContextCallable<V> implements Callable<V> {
-
-    private final Callable<V> delegate;
-    private final Optional<String> optionalTenantIdentifier;
-    private final Optional<UserContext> optionalUserContext;
-
-    DelegatingContextCallable(final Callable<V> delegate, final String tenantIdentifier,
-                              final UserContext userContext) {
-        super();
-        this.delegate = delegate;
-        this.optionalTenantIdentifier = Optional.ofNullable(tenantIdentifier);
-        this.optionalUserContext = Optional.ofNullable(userContext);
-    }
-
-    @Override
-    public V call() throws Exception {
-        try {
-            TenantContextHolder.clear();
-            optionalTenantIdentifier.ifPresent(TenantContextHolder::setIdentifier);
-
-            UserContextHolder.clear();
-            optionalUserContext.ifPresent(UserContextHolder::setUserContext);
-
-            return this.delegate.call();
-        } finally {
-            TenantContextHolder.clear();
-            UserContextHolder.clear();
-        }
-    }
-}
+//package af.asr.async.core;
+//
+//import java.util.Optional;
+//import java.util.concurrent.Callable;
+//
+//@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+//public class DelegatingContextCallable<V> implements Callable<V> {
+//
+//    private final Callable<V> delegate;
+//    private final Optional<String> optionalTenantIdentifier;
+//    private final Optional<UserContext> optionalUserContext;
+//
+//    DelegatingContextCallable(final Callable<V> delegate, final String tenantIdentifier,
+//                              final UserContext userContext) {
+//        super();
+//        this.delegate = delegate;
+//        this.optionalTenantIdentifier = Optional.ofNullable(tenantIdentifier);
+//        this.optionalUserContext = Optional.ofNullable(userContext);
+//    }
+//
+//    @Override
+//    public V call() throws Exception {
+//        try {
+//            TenantContextHolder.clear();
+//            optionalTenantIdentifier.ifPresent(TenantContextHolder::setIdentifier);
+//
+//            UserContextHolder.clear();
+//            optionalUserContext.ifPresent(UserContextHolder::setUserContext);
+//
+//            return this.delegate.call();
+//        } finally {
+//            TenantContextHolder.clear();
+//            UserContextHolder.clear();
+//        }
+//    }
+//}
--- mmo-async/src/main/java/af/asr/async/core/DelegatingContextExecutor.java
+++ mmo-async/src/main/java/af/asr/async/core/DelegatingContextExecutor.java
@@ -1,54 +1,54 @@
-package af.asr.async.core;
-
-
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
-
-import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
-
-public class DelegatingContextExecutor implements AsyncTaskExecutor {
-
-    private final DelegatingSecurityContextAsyncTaskExecutor delegate;
-
-    public DelegatingContextExecutor(final DelegatingSecurityContextAsyncTaskExecutor delegate) {
-        super();
-        this.delegate = delegate;
-    }
-
-    @Override
-    public void execute(final Runnable task, final long startTimeout) {
-        final Runnable taskWrapper = this.wrap(task);
-        this.delegate.execute(taskWrapper, startTimeout);
-    }
-
-    @Override
-    public Future<?> submit(final Runnable task) {
-        final Runnable taskWrapper = this.wrap(task);
-        return this.delegate.submit(taskWrapper);
-    }
-
-    @Override
-    public <T> Future<T> submit(final Callable<T> task) {
-        final Callable<T> taskWrapper = this.wrap(task);
-        return this.delegate.submit(taskWrapper);
-    }
-
-    @Override
-    public void execute(final Runnable task) {
-        final Runnable taskWrapper = this.wrap(task);
-        this.delegate.execute(taskWrapper);
-    }
-
-    private Runnable wrap(final Runnable task) {
-        final String tenantIdentifier = TenantContextHolder.identifier().orElse(null);
-        final UserContext userContext = UserContextHolder.getUserContext().orElse(null);
-        return new DelegatingContextRunnable(task, tenantIdentifier, userContext);
-    }
-
-    private <T> Callable<T> wrap(final Callable<T> task) {
-        final String tenantIdentifier = TenantContextHolder.identifier().orElse(null);
-        final UserContext userContext = UserContextHolder.getUserContext().orElse(null);
-        return new DelegatingContextCallable<>(task, tenantIdentifier, userContext);
-    }
-}
+//package af.asr.async.core;
+//
+//
+//import org.springframework.core.task.AsyncTaskExecutor;
+//import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
+//
+//import java.util.concurrent.Callable;
+//import java.util.concurrent.Future;
+//
+//public class DelegatingContextExecutor implements AsyncTaskExecutor {
+//
+//    private final DelegatingSecurityContextAsyncTaskExecutor delegate;
+//
+//    public DelegatingContextExecutor(final DelegatingSecurityContextAsyncTaskExecutor delegate) {
+//        super();
+//        this.delegate = delegate;
+//    }
+//
+//    @Override
+//    public void execute(final Runnable task, final long startTimeout) {
+//        final Runnable taskWrapper = this.wrap(task);
+//        this.delegate.execute(taskWrapper, startTimeout);
+//    }
+//
+//    @Override
+//    public Future<?> submit(final Runnable task) {
+//        final Runnable taskWrapper = this.wrap(task);
+//        return this.delegate.submit(taskWrapper);
+//    }
+//
+//    @Override
+//    public <T> Future<T> submit(final Callable<T> task) {
+//        final Callable<T> taskWrapper = this.wrap(task);
+//        return this.delegate.submit(taskWrapper);
+//    }
+//
+//    @Override
+//    public void execute(final Runnable task) {
+//        final Runnable taskWrapper = this.wrap(task);
+//        this.delegate.execute(taskWrapper);
+//    }
+//
+//    private Runnable wrap(final Runnable task) {
+//        final String tenantIdentifier = TenantContextHolder.identifier().orElse(null);
+//        final UserContext userContext = UserContextHolder.getUserContext().orElse(null);
+//        return new DelegatingContextRunnable(task, tenantIdentifier, userContext);
+//    }
+//
+//    private <T> Callable<T> wrap(final Callable<T> task) {
+//        final String tenantIdentifier = TenantContextHolder.identifier().orElse(null);
+//        final UserContext userContext = UserContextHolder.getUserContext().orElse(null);
+//        return new DelegatingContextCallable<>(task, tenantIdentifier, userContext);
+//    }
+//}
