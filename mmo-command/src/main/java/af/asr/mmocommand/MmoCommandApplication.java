b7ff7653faeab998d0eb90d7693a4b2bc688cdb4
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-17 14:00:22 +0430

    MMO Crypto Library - Config

---
+++ mmo-command/src/main/java/af/asr/mmocommand/MmoCommandApplication.java
@@ -0,0 +1,13 @@
+package af.asr.mmocommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmoCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmoCommandApplication.class, args);
+	}
+
+}
---
+++ mmo-command/src/test/java/af/asr/mmocommand/MmoCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.mmocommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmoCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
