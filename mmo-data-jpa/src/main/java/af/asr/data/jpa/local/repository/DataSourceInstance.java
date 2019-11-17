a65f5e8661123606bf9568033b0046007ce62c5d
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-17 11:02:46 +0430

    Data JPA Module - Core and Local Data config, migrationi, helper and constants

---
+++ mmo-data-jpa/src/main/java/af/asr/data/jpa/local/repository/DataSourceInstance.java
@@ -0,0 +1,28 @@
+package af.asr.data.jpa.local.repository;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "data_source_instances")
+public class DataSourceInstance {
+
+    @Id
+    @Column(name = "identifier")
+    private String identifier;
+    @Column(name = "driver_class")
+    private String driverClass;
+    @Column(name = "jdbc_url")
+    private String jdbcUrl;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+
+    public DataSourceInstance() {
+        super();
+    }
+
+}
