add777ddcdd918ab613ff551757a94dbda9854ac
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-17 11:40:18 +0430

    PostgreSQL Multitenant Architecture library -- Util Section

---
+++ mmo-postgres/src/main/java/af/asr/postgresql/config/EnablePostgreSQL.java
@@ -0,0 +1,21 @@
+package af.asr.postgresql.config;
+
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@SuppressWarnings({"WeakerAccess", "unused"})
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({PostgreSQLJavaConfigurationImportSelector.class})
+public @interface EnablePostgreSQL {
+    boolean forTenantContext() default true;
+}
