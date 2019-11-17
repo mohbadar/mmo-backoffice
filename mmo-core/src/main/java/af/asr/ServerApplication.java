e7e89d50bc621dd5f4047c1647b3272ed4ad416d
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-17 09:38:56 +0430

    API Library Setup

--- mmo-core/src/main/java/af/asr/ServerApplication.java
+++ mmo-core/src/main/java/af/asr/ServerApplication.java
@@ -6,11 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServerApplication {
 
-	static TestData testData = new TestData();
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-		System.out.print("TestData:   "+ testData.message);
+//		System.out.print("TestData:   "+ testData.message);
 	}
 
 
