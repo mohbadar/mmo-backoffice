75c426d3e835cd0879b171e090f681973b373cb8
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-17 14:05:53 +0430

    MMO Command Library - Setup

---
+++ mmo-command/src/main/java/af/asr/command/annotation/Aggregate.java
@@ -0,0 +1,16 @@
+package af.asr.command.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Component
+public @interface Aggregate {
+}
