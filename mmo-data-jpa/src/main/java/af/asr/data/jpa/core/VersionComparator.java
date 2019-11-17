e940c2ab962ebd38c6efe3054ad78b06d2604b95
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-17 10:57:37 +0430

    Data JPA Module

--- mmo-data-jpa/src/main/java/af/asr/data/jpa/core/VersionComparator.java
+++ mmo-data-jpa/src/main/java/af/asr/data/jpa/core/VersionComparator.java
@@ -1,4 +1,33 @@
 package af.asr.data.jpa.core;
 
+
+import liquibase.changelog.ChangeSet;
+
 public class VersionComparator {
+
+    private VersionComparator() {
+        super();
+    }
+
+    public static int compare(final ChangeSet firstChangeSet, final ChangeSet secondChangeSet) {
+        final String firstId = firstChangeSet.getId();
+        final String secondId = secondChangeSet.getId();
+
+        final String[] splitFirstId = firstId.split("\\.");
+        final String[] splitSecondId = secondId.split("\\.");
+
+        int index = 0;
+
+        while (index < splitFirstId.length
+                && index < splitSecondId.length
+                && splitFirstId[index].equals(splitSecondId[index])) {
+            index++;
+        }
+
+        if (index < splitFirstId.length && index < splitSecondId.length) {
+            return Integer.valueOf(splitFirstId[index]).compareTo(Integer.valueOf(splitSecondId[index]));
+        }
+
+        return Integer.valueOf(splitFirstId.length).compareTo(Integer.valueOf(splitSecondId.length));
+    }
 }
---
+++ mmo-data-jpa/src/main/java/af/asr/data/jpa/local/LocalDatabaseConfiguration.java
@@ -0,0 +1,40 @@
+package af.asr.data.jpa.local;
+
+import org.apache.fineract.cn.core.data.jpa.core.DatabaseConnectorConstants;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Profile("local")
+@Configuration
+@EnableConfigurationProperties({
+        LocalDatabaseProperties.class
+})
+@EnableJpaRepositories(
+        basePackages = {
+                "org.apache.fineract.cn.core.data.jpa.local.repository"
+        }
+)
+public class LocalDatabaseConfiguration {
+
+    public LocalDatabaseConfiguration() {
+        super();
+    }
+
+    @Bean
+    public DataSource dataSource(@Qualifier(DatabaseConnectorConstants.LOGGER_NAME) final Logger logger,
+                                 final LocalDatabaseProperties localDatabaseProperties) {
+        final LocalRoutingDataSource localRoutingDataSource =
+                new LocalRoutingDataSource(logger, localDatabaseProperties);
+        localRoutingDataSource.setTargetDataSources(new HashMap<>());
+        localRoutingDataSource.setLenientFallback(false);
+        return localRoutingDataSource;
+    }
+}
