f7b13098dfc13d274487546e3bb3e45436c39845
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-17 14:09:06 +0430

    MMO Command Library - Annotations

---
+++ mmo-command/src/main/java/af/asr/command/annotation/EventEmitter.java
@@ -0,0 +1,15 @@
+package af.asr.command.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface EventEmitter {
+    String selectorName();
+    String selectorValue();
+}
