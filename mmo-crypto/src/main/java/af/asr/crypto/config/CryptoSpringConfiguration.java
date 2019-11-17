65fd05c4c2052dd7dfe8ebbe103eef418901dfba
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-17 13:46:32 +0430

    MMO Crypto Library - Config

--- mmo-crypto/src/main/java/af/asr/crypto/config/CryptoSpringConfiguration.java
+++ mmo-crypto/src/main/java/af/asr/crypto/config/CryptoSpringConfiguration.java
@@ -1,4 +1,22 @@
 package af.asr.crypto.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = {
+                "af.asr.crypto"
+        },
+        excludeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        value = {
+                                CryptoSpringConfiguration.class
+                        }
+                )
+        }
+)
 public class CryptoSpringConfiguration {
 }
