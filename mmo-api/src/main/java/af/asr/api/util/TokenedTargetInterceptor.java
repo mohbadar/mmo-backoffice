4d5bcd6c7622f1b78de041641e10caeef5221461
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-17 10:32:08 +0430

    API Library Setup - Api Cookie Management

--- mmo-api/src/main/java/af/asr/api/util/TokenedTargetInterceptor.java
+++ mmo-api/src/main/java/af/asr/api/util/TokenedTargetInterceptor.java
@@ -1,2 +1,19 @@
 package af.asr.api.util;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+
+@SuppressWarnings("WeakerAccess")
+public class TokenedTargetInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(final RequestTemplate template) {
+        UserContextHolder.getUserContext()
+                .map(UserContext::getAccessToken)
+                .ifPresent(token -> template.header(ApiConstants.AUTHORIZATION_HEADER, token));
+        UserContextHolder.getUserContext()
+                .map(UserContext::getUser)
+                .ifPresent(user -> template.header(ApiConstants.USER_HEADER, user));
+    }
+}
