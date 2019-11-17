aa838076bd865be8a17f532e6bcd5d70302e681e
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-17 14:38:12 +0430

    MMO Command Library - Domain and Config

---
+++ mmo-command/src/main/java/af/asr/command/gateway/CommandGateway.java
@@ -0,0 +1,27 @@
+package af.asr.command.gateway;
+
+
+import af.asr.command.domain.CommandCallback;
+import af.asr.command.domain.CommandProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommandGateway {
+
+    private final CommandBus commandBus;
+
+    @Autowired
+    public CommandGateway(final CommandBus commandBus) {
+        super();
+        this.commandBus = commandBus;
+    }
+
+    public <C> void process(final C command) {
+        this.commandBus.dispatch(command);
+    }
+
+    public <C, T> CommandCallback<T> process(final C command, Class<T> clazz) throws CommandProcessingException {
+        return new CommandCallback<>(this.commandBus.dispatch(command, clazz));
+    }
+}
