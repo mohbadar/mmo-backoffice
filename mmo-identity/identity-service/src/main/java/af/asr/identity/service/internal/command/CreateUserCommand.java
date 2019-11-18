bf8702d950fe96a209b344810d7e52d791e41666
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-18 15:58:16 +0430

    Identity Service - Commands & Handlers

--- mmo-identity/identity-service/src/main/java/af/asr/identity/service/internal/command/CreateUserCommand.java
+++ mmo-identity/identity-service/src/main/java/af/asr/identity/service/internal/command/CreateUserCommand.java
@@ -1,4 +1,53 @@
 package af.asr.identity.service.internal.command;
 
+import af.asr.identity.api.v1.domain.UserWithPassword;
+
+@SuppressWarnings("unused")
 public class CreateUserCommand {
+    private String identifier;
+    private String role;
+
+    //transient to ensure this field doesn't land in the audit log.
+    private transient String password;
+
+    public CreateUserCommand() {
+    }
+
+    public CreateUserCommand(final UserWithPassword instance) {
+        this.identifier = instance.getIdentifier();
+        this.role = instance.getRole();
+        this.password = instance.getPassword();
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateUserCommand{" +
+                "identifier='" + identifier + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
