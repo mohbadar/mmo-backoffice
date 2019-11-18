ab44a5480c2c60e9d66b0432df48e54da6244975
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-18 15:15:34 +0430

    Identity Service -Repository and Service

--- mmo-identity/identity-service/src/main/java/af/asr/identity/service/internal/service/RoleService.java
+++ mmo-identity/identity-service/src/main/java/af/asr/identity/service/internal/service/RoleService.java
@@ -1,4 +1,45 @@
 package af.asr.identity.service.internal.service;
 
+
+import af.asr.identity.api.v1.domain.Role;
+import af.asr.identity.service.internal.repository.RoleEntity;
+import af.asr.identity.service.internal.repository.Roles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+@Service
 public class RoleService {
+
+    private final Roles repository;
+
+    @Autowired
+    public RoleService(final Roles repository) {
+        this.repository = repository;
+    }
+
+    public List<Role> findAll() {
+        return repository.getAll().stream()
+                .map(this::mapEntity)
+                .sorted((x, y) -> (x.getIdentifier().compareTo(y.getIdentifier())))
+                .collect(Collectors.toList());
+    }
+
+    private Role mapEntity(final RoleEntity roleEntity) {
+        final Role ret = new Role();
+        ret.setIdentifier(roleEntity.getIdentifier());
+        ret.setPermissions(RoleMapper.mapPermissions(roleEntity.getPermissions()));
+        return ret;
+    }
+
+    public Optional<Role> findByIdentifier(final String identifier)
+    {
+        final Optional<RoleEntity> ret = repository.get(identifier);
+
+        return ret.map(this::mapEntity);
+    }
 }
