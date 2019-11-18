95506b3b0c028380218cce5f6e611e9b12f4b233
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-18 11:24:50 +0430

    MMO Identity Service - Client Section

---
+++ mmo-identity/identity-api/src/main/java/af/asr/identity/api/v1/client/ApplicationPermissionAlreadyExistsException.java
@@ -0,0 +1,5 @@
+package af.asr.identity.api.v1.client;
+
+@SuppressWarnings("WeakerAccess")
+public class ApplicationPermissionAlreadyExistsException extends RuntimeException {
+}
---
+++ mmo-identity/identity-api/src/main/java/af/asr/identity/api/v1/client/CallEndpointSetAlreadyExistsException.java
@@ -0,0 +1,5 @@
+package af.asr.identity.api.v1.client;
+
+@SuppressWarnings("WeakerAccess")
+public class CallEndpointSetAlreadyExistsException extends RuntimeException {
+}
