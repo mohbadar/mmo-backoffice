d65d7d6fb9fac0c9020f6944dcd797308ea904ae
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-18 16:03:25 +0430

    Identity Service - Commands & Handlers

--- mmo-identity/identity-service/src/main/java/af/asr/identity/service/internal/command/PasswordAuthenticationCommand.java
+++ mmo-identity/identity-service/src/main/java/af/asr/identity/service/internal/command/PasswordAuthenticationCommand.java
@@ -1,4 +1,39 @@
 package af.asr.identity.service.internal.command;
 
+@SuppressWarnings("unused")
 public class PasswordAuthenticationCommand {
+    private String useridentifier;
+    private transient String password;
+
+    PasswordAuthenticationCommand() {}
+
+    public PasswordAuthenticationCommand(
+            final String useridentifier,
+            final String password) {
+        this.useridentifier = useridentifier;
+        this.password = password;
+    }
+
+    public String getUseridentifier() {
+        return useridentifier;
+    }
+
+    public void setUseridentifier(String useridentifier) {
+        this.useridentifier = useridentifier;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "PasswordAuthenticationCommand{" +
+                "useridentifier='" + useridentifier + '\'' +
+                '}';
+    }
 }
