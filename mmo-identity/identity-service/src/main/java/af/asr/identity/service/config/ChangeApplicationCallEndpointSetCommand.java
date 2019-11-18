efbf5dafc1ea6ffc7cfcb0fc996bcc7660de106c
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-18 15:49:36 +0430

    Identity Service -Mappers

---
+++ mmo-identity/identity-service/src/main/java/af/asr/identity/service/config/ChangeApplicationCallEndpointSetCommand.java
@@ -0,0 +1,54 @@
+package af.asr.identity.service.config;
+
+import af.asr.identity.api.v1.domain.CallEndpointSet;
+
+@SuppressWarnings("unused")
+public class ChangeApplicationCallEndpointSetCommand {
+    private String applicationIdentifier;
+    private String callEndpointSetIdentifier;
+    private CallEndpointSet callEndpointSet;
+
+    public ChangeApplicationCallEndpointSetCommand() {
+    }
+
+    public ChangeApplicationCallEndpointSetCommand(
+            String applicationIdentifier,
+            String callEndpointSetIdentifier,
+            CallEndpointSet callEndpointSet) {
+        this.applicationIdentifier = applicationIdentifier;
+        this.callEndpointSetIdentifier = callEndpointSetIdentifier;
+        this.callEndpointSet = callEndpointSet;
+    }
+
+    public String getApplicationIdentifier() {
+        return applicationIdentifier;
+    }
+
+    public void setApplicationIdentifier(String applicationIdentifier) {
+        this.applicationIdentifier = applicationIdentifier;
+    }
+
+    public String getCallEndpointSetIdentifier() {
+        return callEndpointSetIdentifier;
+    }
+
+    public void setCallEndpointSetIdentifier(String callEndpointSetIdentifier) {
+        this.callEndpointSetIdentifier = callEndpointSetIdentifier;
+    }
+
+    public CallEndpointSet getCallEndpointSet() {
+        return callEndpointSet;
+    }
+
+    public void setCallEndpointSet(CallEndpointSet callEndpointSet) {
+        this.callEndpointSet = callEndpointSet;
+    }
+
+    @Override
+    public String toString() {
+        return "ChangeApplicationCallEndpointSetCommand{" +
+                "applicationIdentifier='" + applicationIdentifier + '\'' +
+                ", callEndpointSetIdentifier='" + callEndpointSetIdentifier + '\'' +
+                '}';
+    }
+}
