6ce7fd62117013b61dc34e90005e7ade78a57404
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-18 15:50:48 +0430

    Identity Service - Commands & Handlers

--- mmo-identity/identity-service/src/main/java/af/asr/identity/service/config/ChangeRoleCommand.java
+++ mmo-identity/identity-service/src/main/java/af/asr/identity/service/config/ChangeRoleCommand.java
@@ -1,4 +1,42 @@
 package af.asr.identity.service.config;
 
+
+import af.asr.identity.api.v1.domain.Role;
+
+@SuppressWarnings("unused")
 public class ChangeRoleCommand {
+    private String identifier;
+    private Role instance;
+
+    public ChangeRoleCommand()
+    {
+    }
+
+    public ChangeRoleCommand(final String identifier, final Role instance) {
+        this.identifier = identifier;
+        this.instance = instance;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public Role getInstance() {
+        return instance;
+    }
+
+    public void setInstance(Role instance) {
+        this.instance = instance;
+    }
+
+    @Override
+    public String toString() {
+        return "ChangeRoleCommand{" +
+                "identifier='" + identifier + '\'' +
+                '}';
+    }
 }
