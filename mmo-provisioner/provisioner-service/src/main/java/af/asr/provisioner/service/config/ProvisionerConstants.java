b73de9b81c24a120c9ea9a045818d7c6c14bed39
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-20 15:27:36 +0430

    Provisioner -Service Module Implementation

---
+++ mmo-provisioner/provisioner-service/src/main/java/af/asr/provisioner/service/config/ProvisionerConstants.java
@@ -0,0 +1,10 @@
+package af.asr.provisioner.service.config;
+
+public interface ProvisionerConstants {
+
+    String LOGGER_NAME = "provisioner-logger";
+    String INITIAL_PWD = "oS/0IiAME/2unkN1momDrhAdNKOhGykYFH/mJN20";
+    String CONFIG_INTERNAL = "org.apache.fineract.cn.provisioner.internal";
+    int ITERATION_COUNT = 4096;
+    int HASH_LENGTH = 256;
+}
