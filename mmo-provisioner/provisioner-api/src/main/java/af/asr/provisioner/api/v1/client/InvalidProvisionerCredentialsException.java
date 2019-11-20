f2ca0987fa905c786a6c39318e26042c3e19361f
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-20 11:32:31 +0430

    permitted-feign-client Service - Security

---
+++ mmo-provisioner/provisioner-api/src/main/java/af/asr/provisioner/api/v1/client/InvalidProvisionerCredentialsException.java
@@ -0,0 +1,8 @@
+package af.asr.provisioner.api.v1.client;
+
+@SuppressWarnings("WeakerAccess")
+public class InvalidProvisionerCredentialsException extends RuntimeException {
+    public InvalidProvisionerCredentialsException() {
+        super();
+    }
+}
