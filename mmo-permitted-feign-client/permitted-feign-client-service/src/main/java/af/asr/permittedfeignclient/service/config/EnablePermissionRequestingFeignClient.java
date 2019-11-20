8202c0ed780882459a962f0ab3e8c615b878e898
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-20 10:57:08 +0430

    permitted-feign-client Service - Service Module

--- mmo-permitted-feign-client/permitted-feign-client-service/src/main/java/af/asr/permittedfeignclient/service/config/EnablePermissionRequestingFeignClient.java
+++ mmo-permitted-feign-client/permitted-feign-client-service/src/main/java/af/asr/permittedfeignclient/service/config/EnablePermissionRequestingFeignClient.java
@@ -1,4 +1,22 @@
 package af.asr.permittedfeignclient.service.config;
 
-public class EnablePermissionRequestingFeignClient {
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@SuppressWarnings({"unused", "WeakerAccess"})
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Import({
+        PermittedFeignClientBeanDefinitionRegistrar.class,
+        PermittedFeignClientImportSelector.class,
+        PermittedFeignClientConfiguration.class,
+})
+public @interface EnablePermissionRequestingFeignClient {
+    /**
+     * @return A list of classes annotated with @EndpointSet and @FeignClient
+     */
+    Class<?>[] feignClasses() default {};
 }
