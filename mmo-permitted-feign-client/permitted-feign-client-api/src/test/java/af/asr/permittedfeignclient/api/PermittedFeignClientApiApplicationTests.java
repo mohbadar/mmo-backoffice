ad5cdd2fbe6c9fd93eac8ac1917b239c951e5ebe
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-20 09:57:52 +0430

    permitted-feign-client Service - Setup

---
+++ mmo-permitted-feign-client/permitted-feign-client-api/src/test/java/af/asr/permittedfeignclient/api/PermittedFeignClientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.permittedfeignclient.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermittedFeignClientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mmo-permitted-feign-client/permitted-feign-client-service/src/main/java/af/asr/permittedfeignclient/service/PermittedFeignClientServiceApplication.java
@@ -0,0 +1,13 @@
+package af.asr.permittedfeignclient.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermittedFeignClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermittedFeignClientServiceApplication.class, args);
+	}
+
+}
