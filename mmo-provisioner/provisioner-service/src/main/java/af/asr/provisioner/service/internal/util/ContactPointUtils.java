4d8bc091469950aece1a535d15058de47ef0b7cc
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-20 15:47:54 +0430

    Provisioner -Service Module Implementation

---
+++ mmo-provisioner/provisioner-service/src/main/java/af/asr/provisioner/service/internal/util/ContactPointUtils.java
@@ -0,0 +1,31 @@
+package af.asr.provisioner.service.internal.util;
+
+import com.datastax.driver.core.Cluster;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+public class ContactPointUtils {
+
+    private ContactPointUtils() {
+        super();
+    }
+
+    public static void process(final Cluster.Builder clusterBuilder, final String contactPoints) {
+        final String[] splitContactPoints = contactPoints.split(",");
+        for (final String contactPoint : splitContactPoints) {
+            if (contactPoint.contains(":")) {
+                final String[] address = contactPoint.split(":");
+                clusterBuilder.addContactPointsWithPorts(
+                        new InetSocketAddress(address[0].trim(), Integer.valueOf(address[1].trim())));
+            } else {
+                try {
+                    clusterBuilder.addContactPoints(InetAddress.getByName(contactPoint.trim()));
+                } catch (final UnknownHostException uhex) {
+                    throw new IllegalArgumentException("Host not found!", uhex);
+                }
+            }
+        }
+    }
+}
